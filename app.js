21d8cdad7f15315994fb8273c382f202f2b9544d
daulatrao7 <66482182+daulatrao7@users.noreply.github.com>
2020-12-20 22:42:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-var btntranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputdiv = document.querySelector("#output");
+var btnTranslate= document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
 
-var serverURL= "https://api.funtranslations.com/translate/pig-latin.json"
+
+var serverURL="https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text){
-    return serverURL + "?" + "text" + text
+    return serverURL+ "?"+ "text="+text;
+
 }
 
 function errorHandler(error){
-    console.log("error occured", error);
-    alert("something is wrong with the server")
+    console.log("error occured :"+ error)
+    alert(("Server error, Try after some time. error type:", error))
 }
 
-function clickHandler(){
-    var inputText = txtInput.value;
+function clickEvenetHandler(){
+    console.log("Input detected:")
+    console.log(txtInput.value);
 
-    fetch(getTranslationURL(inputText))
-    .then(Response => Response.json())
-    .then(json=> {
-        var translatedText = json.contents.translated;
-        outputdiv.innerText= translatedText;
-    })
+    fetch(getTranslationURL(txtInput.value))
+    .then(response=>response.json()
+    .then(json=>{
+        if(response.status!==200){
+        var err=json.error.message;outputDiv.innerText=err;alert(err)}
+        else{
+        var translatedText=json.contents.translated;
+                    outputDiv.innerText=translatedText;}})
     .catch(errorHandler)
-};
+    );
 
-btntranslate.addEventListener("click", clickHandler)
+
+}
+
+btnTranslate.addEventListener("click",  clickEvenetHandler)
