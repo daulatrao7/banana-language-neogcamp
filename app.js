4538126b584ed5f4dc515dccd2e436a6a10231b4
daulatrao7 <66482182+daulatrao7@users.noreply.github.com>
2020-12-20 12:26:24 +0530

    final

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ var btntranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputdiv = document.querySelector("#output");
 
-function clickhandler(){
-    outputdiv.innerText = "ajajaja " + txtInput.value;
+var serverURL= "https://api.funtranslations.com/translate/pig-latin.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text" + text
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something is wrong with the server")
+}
+
+function clickHandler(){
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(Response => Response.json())
+    .then(json=> {
+        var translatedText = json.contents.translatedText;
+        outputdiv.innerText= translatedText;
+    })
+    .catch(errorHandler)
 };
 
-btntranslate.addEventListener("click", clickhandler)
+btntranslate.addEventListener("click", clickHandler)
